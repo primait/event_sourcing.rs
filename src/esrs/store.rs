6f81856bd0a2ab8e37a0a9165f0e3a7efd8a67da
Simone Cottini <cottini.simone@gmail.com>
2022-10-06 13:25:39 +0200

    Add default implementation of EventStore for every Box<dyn EventStore>

--- src/esrs/store.rs
+++ src/esrs/store.rs
@@ -57,3 +57,57 @@ impl<Event> StoreEvent<Event> {
         &self.payload
     }
 }
+
+/// Default generic implementation for every type [`Box<dyn EventStore>`]. This is particularly useful
+/// when there's the need in your codebase to have a generic [`EventStore`] inside your [`Aggregate`].
+///
+/// # Example
+///
+/// ```ignore
+/// pub struct MyAggregate {
+///     event_store: Box<dyn esrs::EventStore<Manager = Self>>,
+/// }
+///
+/// // Your [`Aggregate`] impl here
+///
+/// impl esrs::AggregateManager for MyAggregate {
+///     type EventStore = Box<dyn esrs::EventStore<Manager = Self>>;
+///
+///     fn name() -> &'static str where Self: Sized {
+///         "whatever"
+///     }
+///
+///     fn event_store(&self) -> &Self::EventStore {
+///         self.event_store.as_ref()
+///     }
+/// }
+/// ```
+#[async_trait]
+impl<M> EventStore for Box<dyn EventStore<Manager = M> + Send + Sync>
+where
+    M: AggregateManager,
+{
+    type Manager = M;
+
+    async fn by_aggregate_id(
+        &self,
+        aggregate_id: Uuid,
+    ) -> Result<Vec<StoreEvent<<Self::Manager as Aggregate>::Event>>, <Self::Manager as Aggregate>::Error> {
+        self.as_ref().by_aggregate_id(aggregate_id).await
+    }
+
+    async fn persist(
+        &self,
+        aggregate_id: Uuid,
+        events: Vec<<Self::Manager as Aggregate>::Event>,
+        starting_sequence_number: SequenceNumber,
+    ) -> Result<Vec<StoreEvent<<Self::Manager as Aggregate>::Event>>, <Self::Manager as Aggregate>::Error> {
+        self.as_ref()
+            .persist(aggregate_id, events, starting_sequence_number)
+            .await
+    }
+
+    async fn delete(&self, aggregate_id: Uuid) -> Result<(), <Self::Manager as Aggregate>::Error> {
+        self.as_ref().delete(aggregate_id).await
+    }
+}
