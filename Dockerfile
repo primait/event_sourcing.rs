b58bd363a3f01e81c925b53b90f32a4caeb3babb
Angelo <angelo.rendina@prima.it>
2022-11-17 10:58:30 +0000

    better example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM public.ecr.aws/prima/rust:1.64.0
+FROM public.ecr.aws/prima/rust:1.65.0-1
 
 # Serve per avere l'owner dei file scritti dal container uguale all'utente Linux sull'host
 USER app
--- examples/pessimistic_lock/src/lib.rs
+++ examples/pessimistic_lock/src/lib.rs
@@ -1,34 +1,64 @@
 use esrs::AggregateManager;
 use uuid::Uuid;
 
-use crate::aggregate::CountingAggregate;
-use crate::aggregate::CountingCommand;
-use crate::aggregate::CountingError;
-
-mod aggregate;
+use delete_aggregate::aggregate::CounterAggregate;
+use delete_aggregate::structs::CounterCommand;
+use delete_aggregate::structs::CounterError;
 
 /// Increment the value behind this `aggregate_id` as soon as atomical access can be obtained.
-/// The lock can be obtained even if there are current optimistic accesses! Avoid mixing the two strategies.
-pub async fn increment_atomically(aggregate: CountingAggregate, aggregate_id: Uuid) -> Result<u64, CountingError> {
+/// The lock can be obtained even if there are current optimistic accesses! Avoid mixing the two strategies when writing.
+pub async fn increment_atomically(aggregate: CounterAggregate, aggregate_id: Uuid) -> Result<(), CounterError> {
     // Obtain a lock for this aggregate_id, or wait for an existing one to be released.
     let _guard = aggregate.lock(aggregate_id).await?;
     // Only one atomical access at a time can proceed further.
     let aggregate_state = aggregate.load(aggregate_id).await.unwrap_or_default();
-    let new_state = aggregate
-        .handle_command(aggregate_state, CountingCommand::Increment)
+    aggregate
+        .handle_command(aggregate_state, CounterCommand::Increment)
         .await?;
-    Ok(*new_state.inner())
+    Ok(())
 }
 
 /// Increment the value behind this `aggregate_id` with an optimistic locking strategy.
-/// Optimistic access ignores any current active lock! Avoid mixing the two strategies.
-pub async fn increment_optimistically(aggregate: CountingAggregate, aggregate_id: Uuid) -> Result<u64, CountingError> {
+/// Optimistic access ignores any current active lock! Avoid mixing the two strategies when writing.
+pub async fn increment_optimistically(aggregate: CounterAggregate, aggregate_id: Uuid) -> Result<(), CounterError> {
     // Every optimistic access can take place concurrently...
     let aggregate_state = aggregate.load(aggregate_id).await.unwrap_or_default();
     // ...and events are persisted in non-deterministic order.
     // This could raise optimistic locking errors, that must be handled downstream.
-    let new_state = aggregate
-        .handle_command(aggregate_state, CountingCommand::Increment)
+    aggregate
+        .handle_command(aggregate_state, CounterCommand::Increment)
         .await?;
-    Ok(*new_state.inner())
+    Ok(())
+}
+
+/// Load the aggregate state for read-only purposes, preventing others (that use locking) from modifying it.
+/// Avoid using atomic reads if writes are optimistic, as the state would be modified anyway!
+/// If writes are atomic, it is perfectly fine to use a mixture of atomic and optimistic reads.
+pub async fn with_atomic_read(aggregate: CounterAggregate, aggregate_id: Uuid) -> Result<(), CounterError> {
+    // Obtain a lock for this aggregate_id, or wait for an existing one to be released.
+    let _guard = aggregate.lock(aggregate_id).await?;
+    // Only one atomical access at a time can proceed further.
+    let aggregate_state = aggregate.load(aggregate_id).await.unwrap_or_default();
+    // No one else (employing locking!) can read or modify the state just loaded here,
+    // ensuring this really is the *latest* aggregate state.
+    println!(
+        "Doing something with the state just read: {}",
+        aggregate_state.next_sequence_number()
+    );
+    Ok(())
+}
+
+/// Load the aggregate state for read-only purposes, optimistically assuming nothing is modifying it.
+/// If writes are atomic, it is perfectly fine to use a mixture of atomic and optimistic reads.
+/// Otherwise, optimistic reads are fine: beware there are no guarantees the state loaded is actually the latest.
+pub async fn with_optimistic_read(aggregate: CounterAggregate, aggregate_id: Uuid) -> Result<(), CounterError> {
+    // Read the state now, ignoring any explicit locking...
+    let aggregate_state = aggregate.load(aggregate_id).await.unwrap_or_default();
+    // ...but nothing prevents something else from updating the data in the store in the meanwhile,
+    // so the `aggregate_state` here might be already outdated at this point.
+    println!(
+        "Doing something with the state just read: {}",
+        aggregate_state.next_sequence_number()
+    );
+    Ok(())
 }
