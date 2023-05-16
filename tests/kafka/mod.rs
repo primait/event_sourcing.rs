cc4fa772d3b38a079294284a73936dbff44d57d3
Simone Cottini <cottini.simone@gmail.com>
2023-05-16 14:18:09 +0200

    WIP trying to let kafka tests work..

--- tests/kafka/mod.rs
+++ tests/kafka/mod.rs
@@ -1,4 +1,8 @@
 use chrono::Utc;
+use rdkafka::admin::{AdminOptions, NewTopic, TopicReplication};
+use rdkafka::config::FromClientConfig;
+use rdkafka::consumer::{CommitMode, Consumer, StreamConsumer};
+use rdkafka::ClientConfig;
 use uuid::Uuid;
 
 use esrs::event_bus::kafka::{KafkaEventBus, KafkaEventBusConfig};
@@ -10,10 +14,13 @@ use crate::aggregate::{TestAggregate, TestEvent};
 #[tokio::test]
 async fn kafka_event_bus_test() {
     let kafka_broker_url: String = std::env::var("KAFKA_BROKERS_URL").unwrap();
+    let topic: &str = "test";
+
+    create_topic(kafka_broker_url.as_str(), topic).await;
 
     let config: KafkaEventBusConfig = KafkaEventBusConfig::builder()
         .broker_url_list(kafka_broker_url.as_str())
-        .topic("test")
+        .topic(topic)
         .error_handler(Box::new(|error| panic!("{:?}", error)))
         .build();
 
@@ -31,6 +38,46 @@ async fn kafka_event_bus_test() {
     };
 
     bus.publish(&store_event).await;
+
+    let consumer = consumer(kafka_broker_url.as_str(), topic);
+
+    match consumer.recv().await {
+        Err(e) => panic!("Kafka error: {}", e),
+        Ok(m) => {
+            consumer.commit_message(&m, CommitMode::Async).unwrap();
+        }
+    };
 }
 
-fn consumer() {}
+fn consumer(broker_urls: &str, topic: &str) -> StreamConsumer {
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", "1")
+        .set("bootstrap.servers", broker_urls)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("max.poll.interval.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set("allow.auto.create.topics", "true")
+        .create()
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&[topic])
+        .expect("Can't subscribe to specified topics");
+
+    consumer
+}
+
+async fn create_topic(broker_urls: &str, topic: &str) {
+    let mut admin_config = ClientConfig::new();
+    admin_config.set("bootstrap.servers", broker_urls);
+
+    let admin_client = rdkafka::admin::AdminClient::from_config(&admin_config).unwrap();
+
+    let new_topic = NewTopic::new(topic, 1, TopicReplication::Fixed(1));
+
+    let _ = admin_client
+        .create_topics(&[new_topic], &AdminOptions::default())
+        .await
+        .unwrap();
+}
