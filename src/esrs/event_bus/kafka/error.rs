eb4bf58c7ccb40dc9fec539f0b66654cc89d4d0c
Simone Cottini <cottini.simone@gmail.com>
2023-06-09 16:26:04 +0200

    Use thiserror for KafkaEventBusError and RabbitEventBusError

--- src/esrs/event_bus/kafka/error.rs
+++ src/esrs/event_bus/kafka/error.rs
@@ -3,22 +3,12 @@
 /// - `Json`: Indicates a failure in serializing/deserializing the event payload.
 /// - `Kafka`: Indicates an error occurred while establishing a connection with the Kafka cluster or
 ///            an error encountered during the event publishing process.
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum KafkaEventBusError {
-    Json(serde_json::Error),
-    Kafka(rdkafka::error::KafkaError),
-}
-
-impl From<serde_json::Error> for KafkaEventBusError {
-    fn from(value: serde_json::Error) -> Self {
-        Self::Json(value)
-    }
-}
-
-impl From<rdkafka::error::KafkaError> for KafkaEventBusError {
-    fn from(value: rdkafka::error::KafkaError) -> Self {
-        Self::Kafka(value)
-    }
+    #[error(transparent)]
+    Json(#[from] serde_json::Error),
+    #[error(transparent)]
+    Kafka(#[from] rdkafka::error::KafkaError),
 }
 
 impl From<(rdkafka::error::KafkaError, rdkafka::message::OwnedMessage)> for KafkaEventBusError {
